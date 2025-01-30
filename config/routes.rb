dd918773abef7dd4b9989485ef86595c4eada492
Mark Perkins <mark@allmarkedup.com>
2025-01-30 17:19:56 +0000

    Fix homepage redirect

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", :as => :rails_health_check
 
-  root "crosswords/quick#index"
+  root "crosswords#index"
 
   get ":style", to: "crosswords#index", constraints: {style: /quick/}, as: :crosswords
   get ":style/:number", to: "crosswords#show", constraints: {number: /\d.+/, style: /quick/}, as: :crossword
