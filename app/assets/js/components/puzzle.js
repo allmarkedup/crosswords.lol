d7d4a5f2a2c100514324fdfc2d6741d70b93c04c
Mark Perkins <mark@allmarkedup.com>
2025-02-04 17:16:22 +0000

    Improve confetti

--- app/assets/js/components/puzzle.js
+++ app/assets/js/components/puzzle.js
@@ -1,73 +1,127 @@
-import { tsParticles } from "@tsparticles/engine";
-import { loadConfettiPreset } from "@tsparticles/preset-confetti";
-import { loadEmojiShape } from "@tsparticles/shape-emoji";
-import { loadImageShape } from "@tsparticles/shape-image";
+const confettiCount = 200;
+const confettiDefaults = {
+  spread: 70,
+  angle: -90,
+  origin: { x: 0.5, y: 0 },
+  shapes: ["emoji", "image"],
+  shapeOptions: {
+    emoji: {
+      value: ["🦄", "🌈", "⭐️"],
+      particles: {
+        size: {
+          value: 8,
+        },
+      },
+    },
+    image: [
+      {
+        src: RAILS_ASSET_URL("icon.svg"),
+        width: 32,
+        height: 32,
+        particles: {
+          size: {
+            value: 10,
+          },
+        },
+      },
+    ],
+  },
+};
+
+function fireConfetti(particleRatio, opts) {
+  window.confetti(
+    Object.assign({}, confettiDefaults, opts, {
+      particleCount: Math.floor(confettiCount * particleRatio),
+    })
+  );
+}
 
 const puzzle = function (args) {
   return {
     entries: args.entries,
     activeEntryId: args.entries[0].id,
 
-    async init() {
-      await loadConfettiPreset(tsParticles);
-      await loadEmojiShape(tsParticles);
-      await loadImageShape(tsParticles);
-    },
+    showConfetti() {
+      fireConfetti(0.25, {
+        spread: 26,
+        startVelocity: 55,
+      });
 
-    async showConfetti() {
-      return tsParticles.load({
-        id: "confetti",
-        options: {
-          emitters: {
-            startCount: 150,
-            position: {
-              x: 50,
-              y: 0,
-            },
-            size: {
-              width: 0,
-              height: 0,
-            },
-            rate: {
-              delay: 10,
-              quantity: 0,
-            },
-            life: {
-              duration: 0.1,
-              count: 1,
-            },
-          },
-          particles: {
-            shape: {
-              type: ["image", "emoji"],
-              options: {
-                emoji: {
-                  particles: {
-                    size: {
-                      value: 10,
-                    },
-                  },
-                  value: ["🦄", "⭐️", "🌈", "🧩"],
-                },
-                image: [
-                  {
-                    src: RAILS_ASSET_URL("icon.svg"),
-                    width: 32,
-                    height: 32,
-                    particles: {
-                      size: {
-                        value: 14,
-                      },
-                    },
-                  },
-                ],
-              },
-            },
-          },
-          preset: "confetti",
-        },
+      fireConfetti(0.2, {
+        spread: 60,
+      });
+
+      fireConfetti(0.35, {
+        spread: 100,
+        decay: 0.91,
+        scalar: 0.8,
+      });
+
+      fireConfetti(0.1, {
+        spread: 120,
+        startVelocity: 25,
+        decay: 0.92,
+        scalar: 1.2,
+      });
+
+      fireConfetti(0.1, {
+        spread: 120,
+        startVelocity: 45,
       });
     },
+    //   return tsParticles.load({
+    //     id: "confetti",
+    //     options: {
+    //       emitters: {
+    //         startCount: 150,
+    //         position: {
+    //           x: 50,
+    //           y: 0,
+    //         },
+    //         size: {
+    //           width: 0,
+    //           height: 0,
+    //         },
+    //         rate: {
+    //           delay: 10,
+    //           quantity: 0,
+    //         },
+    //         life: {
+    //           duration: 0.1,
+    //           count: 1,
+    //         },
+    //       },
+    //       particles: {
+    //         shape: {
+    //           type: ["image", "emoji"],
+    //           options: {
+    //             emoji: {
+    //               particles: {
+    //                 size: {
+    //                   value: 10,
+    //                 },
+    //               },
+    //               value: ["🦄", "⭐️", "🌈", "🧩"],
+    //             },
+    //             image: [
+    //               {
+    //                 src: RAILS_ASSET_URL("icon.svg"),
+    //                 width: 32,
+    //                 height: 32,
+    //                 particles: {
+    //                   size: {
+    //                     value: 14,
+    //                   },
+    //                 },
+    //               },
+    //             ],
+    //           },
+    //         },
+    //       },
+    //       preset: "confetti",
+    //     },
+    //   });
+    // },
 
     get activeEntry() {
       if (!this.activeEntryId) return null;
