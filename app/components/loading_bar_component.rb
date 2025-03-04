37b08b37d7c317e7ff3783b0e724dcce8868945a
Mark Perkins <mark@allmarkedup.com>
2025-03-04 17:45:48 +0000

    Inline loading bar and settings component HTML

--- app/components/loading_bar_component.rb
+++
@@ -1,2 +0,0 @@
-class LoadingBarComponent < BaseComponent
-end
--- app/components/settings_component.rb
+++
@@ -1,7 +0,0 @@
-class SettingsComponent < BaseComponent
-  attr_reader :back
-
-  def initialize(back: nil)
-    @back = back
-  end
-end
