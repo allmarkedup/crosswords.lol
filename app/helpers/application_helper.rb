156cb55f1767362ff33b881c386caf535e1ba07b
Mark Perkins <mark@allmarkedup.com>
2025-03-12 19:18:52 +0000

    Make about page less cringe

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,21 @@
 module ApplicationHelper
+  def crosswords_dot_lol
+    link_to root_path, class: "crosswords-dot-lol" do
+      safe_join([
+        image_tag("icon.svg", alt: "Crosswords.lol logo"),
+        tag.span("crosswords.lol")
+      ])
+    end
+  end
+
+  def recent_crossword_path
+    session[:latest_crossword_number] ? crossword_path(session[:latest_crossword_number]) : root_path
+  end
+
   def back_to_crosswords_link
     link_to(
       "Back to the crossword#{"s" unless session[:latest_crossword_number]}",
-      session[:latest_crossword_number] ? crossword_path(session[:latest_crossword_number]) : root_path,
+      recent_crossword_path,
       "@click.prevent": "hijax"
     )
   end
