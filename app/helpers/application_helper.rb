fe3ca6aa8ceccaafd61f1bd6a6bc8f7f0abb54e7
Mark Perkins <mark@allmarkedup.com>
2025-03-12 15:29:54 +0000

    Hijax up nav and page links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,8 @@ module ApplicationHelper
   def back_to_crosswords_link
     link_to(
       "Back to the crossword#{"s" unless session[:latest_crossword_number]}",
-      session[:latest_crossword_number] ? crossword_path(session[:latest_crossword_number]) : root_path
+      session[:latest_crossword_number] ? crossword_path(session[:latest_crossword_number]) : root_path,
+      "@click.prevent": "hijax"
     )
   end
 end
