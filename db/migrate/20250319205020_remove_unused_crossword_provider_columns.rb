019cb0ca7d3d47caf1da133ee08b5c07f34c80cf
Mark Perkins <mark@allmarkedup.com>
2025-03-19 21:03:40 +0000

    Add column type to migration

--- db/migrate/20250319205020_remove_unused_crossword_provider_columns.rb
+++ db/migrate/20250319205020_remove_unused_crossword_provider_columns.rb
@@ -1,6 +1,6 @@
 class RemoveUnusedCrosswordProviderColumns < ActiveRecord::Migration[8.0]
   def change
-    remove_column :crosswords, :provider_name
-    remove_column :crosswords, :provider_reference
+    remove_column :crosswords, :provider_name, :string
+    remove_column :crosswords, :provider_reference, :string
   end
 end
